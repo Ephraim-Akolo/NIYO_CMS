0fd51d9ef60a77f3bfe98becd047c738112dc01d
Jake-Ephraim <ephraimakolo2017@gmail.com>
2024-05-19 22:44:12 +0100

    added custom User class

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'authentication.User'
+
 REST_FRAMEWORK = { 
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema', 
     'DEFAULT_AUTHENTICATION_CLASSES': (
