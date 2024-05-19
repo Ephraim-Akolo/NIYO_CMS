1134f484cc0a7dd1bcc06c9849ab055726381ccd
Jake-Ephraim <ephraimakolo2017@gmail.com>
2024-05-19 23:06:18 +0100

    added task model

--- taskManager/models.py
+++ taskManager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+
+class Task(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    task = models.TextField()
+    completed = models.BooleanField(default=False)
+    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
